49b7a5577c3447710c3ed8d074ff54979a5664c8
mcmarius <23401453+mcmarius@users.noreply.github.com>
2023-02-27 10:54:15 +0200

    Getteri și setteri

--- main.cpp
+++ main.cpp
@@ -1,9 +1,44 @@
 #include <iostream>
+#include <vector>
+
+class Facultate {
+//    std::vector<Student> studenti;
+};
+
+class Student {
+//private:
+    Facultate facultate;
+    std::string nume;
+    int grupa;
+public:
+    void setFacultate(Facultate f) {
+        facultate = f;
+//        this->facultate = f;
+//        Student::facultate = f;
+    }
+    void setNume(std::string nume_) {
+        nume = nume_;
+    }
+    void setGrupa(int gr) {
+        grupa = gr;
+    }
+    Facultate getFacultate() { return facultate; }
+    std::string getNume() { return nume; }
+    int getGrupa() { return grupa; }
+};
 
 int main() {
-//    std::cout << "Hello, 1511!\n";
-    std::string mesaj;
-    std::cin >> mesaj;
-    std::cout << "am citit " << mesaj << "\n";
+    Facultate fmi;
+    Facultate poli, ase;
+    poli = ase;
+
+    Student st;
+//    st.facultate = fmi;
+    st.setFacultate(fmi);
+    std::cout << st.getNume() << " " << st.getGrupa() << "\n";
+    st.setGrupa(1511);
+    std::cout << st.getNume() << " " << st.getGrupa() << "\n";
+    st.setNume("test");
+    std::cout << st.getNume() << " " << st.getGrupa() << "\n";
     return 0;
 }
