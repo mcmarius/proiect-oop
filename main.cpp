85c3eb1cb1036bfce48b3b0b8e02549c39d4d19d
mcmarius <23401453+mcmarius@users.noreply.github.com>
2023-02-27 10:32:46 +0200

    Citire de la tastatură

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, 1511!\n";
+//    std::cout << "Hello, 1511!\n";
+    std::string mesaj;
+    std::cin >> mesaj;
+    std::cout << "am citit " << mesaj << "\n";
     return 0;
 }
